c7ba6f48770d05db2d240c9e70dfa03015427290
Rohan Kumar Mahto <rohankumar993985@gmail.com>
2025-10-09 13:12:30 +0530

    feat: implement logout functionality with API integration and user feedback

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+    try {
+
+        const response = NextResponse.json({ message: "Logout successful", success: true }, { status: 200 });
+        response.cookies.set("token", "", { httpOnly: true, path: "/", expires: new Date(0) });
+        return response;
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error.message
+        }, {
+            status: 500
+        })
+    }
+
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,33 @@
+"use client"
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
+
 export default function ProfilePage() {
+
+    const router = useRouter();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const logout = async (e: any) => {
+        e.preventDefault();
+        try {
+            await axios.get("http://localhost:3000/api/users/logout")
+            toast.success("Logged out successfully.")
+            router.push("/login")
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
+
     return (
         <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
             <div className="flex flex-col items-center gap-4 border p-8 rounded">
                 <h1 className="text-2xl font-bold">Profile</h1>
                 <p>Welcome to your profile page!</p>
+                <hr />
+                <button onClick={(e) => logout(e)} className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded mt-2 cursor-pointer">Logout</button>
             </div>
         </div>
     )
